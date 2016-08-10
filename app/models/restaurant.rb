258c5dc251979cda275a763f976e3533c083cb7b
Alex <alexduval71@gmail.com>
2016-08-10 12:19:28 +0200

    modify column adress on adress

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validate :address, presence: true
+  validates :category, inclusion: {in: ['chinese', 'italian', 'japanese', 'french', 'belgian'], allow_nil: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810093638) do
+ActiveRecord::Schema.define(version: 20160810101554) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.string   "adress"
+    t.string   "address"
     t.string   "phone"
     t.string   "category"
     t.datetime "created_at", null: false
