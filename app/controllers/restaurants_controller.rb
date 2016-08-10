ee3ba01830b0b7dd2d022467ff9650559789f99f
Alex <alexduval71@gmail.com>
2016-08-10 16:17:51 +0200

    Add new and create restaurant with link from index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,10 +9,20 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurants_path(@restaurant)
+      else
+        render :new
+      end
+  end
 
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
