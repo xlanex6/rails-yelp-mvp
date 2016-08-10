096fce5f6913ad07324aedd3b96517653879da12
Alex <alexduval71@gmail.com>
2016-08-10 14:24:36 +0200

    Add faker and seed fake Restaurant

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create(
+  { name:         Faker::StarWars.planet,
+    address:      Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:     ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  }
+  )
+end
