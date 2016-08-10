4b9ee922339004947c9ea011e772a0c82fdca391
Alex <alexduval71@gmail.com>
2016-08-10 14:06:30 +0200

    All validation model OK

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating , allow_nil: false, presence: true
-  validates :content , allow_nil: false, presence: true
+  validates :rating, presence: true, inclusion: {in: (0..5)}, numericality: true
+  validates :content,  presence: true
 end
